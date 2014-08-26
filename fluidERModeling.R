d85153db28f9ea8684001fae0f5056fd81e5fc72
erichhuang <erich@mac.com>
2014-08-26 14:04:55 -0400

    2nd commit.

---
+++ fluidERModeling.R
@@ -0,0 +1,9 @@
+## fluidERModeling.R
+## erich.huang@duke.edu
+
+## Experimenting with the H2O framework for machine learning
+
+## REQUIRED PACKAGES
+
+## LOAD DATA
+load('~/Quantwork/ARTEMIS/bcSignaling/data/dfciData.RData')
