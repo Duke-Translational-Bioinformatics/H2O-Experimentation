f877d034b88061d5db4841670a54b2a7b033449a
erichhuang <erich@mac.com>
2014-08-27 15:28:24 -0400

    Downsampling the probeset feature space.

--- fluidERModeling.R
+++ fluidERModeling.R
@@ -22,6 +22,12 @@ load('~/Quantwork/ARTEMIS/bcSignaling/data/dfciData.RData')
 metaDF$series <- as.factor(metaDF$series)
 vdxMetaDF <- metaDF[metaDF$series == 'VDX', ]
 vdxExpDF <- fullDF[grep('VDX_', rownames(fullDF)), ]
+
+# Reduce the feature space for testing
+set.seed(140826)
+sampleGenes <- sample(colnames(vdxExpDF), size = 500)
+vdxExpDF <- vdxExpDF[ , sampleGenes]
+
 vdxExpDF <- data.frame(patients = rownames(vdxExpDF), er_status = vdxMetaDF[rownames(vdxExpDF), 'er'], vdxExpDF)
 
 ## DIVIDE DATA INTO TRAIN AND TEST SETS
@@ -32,7 +38,17 @@ testIDs <- setdiff(rownames(vdxExpDF), trainIDs)
 trainData <- vdxExpDF[trainIDs, ]
 testData <- vdxExpDF[testIDs, ]
 
-
 ## PUSH DATA to H2O
 trainBC <- as.h2o(client = localH2O, object = trainData, key = 'trainBC')
 testBC <- as.h2o(client = localH2O, object = testData, key = 'testBC')
+
+## BUILD MODEL
+rfModel <- h2o.randomForest(x = 3:ncol(testBC), 
+                            y = 2,
+                            data = testBC, 
+                            classification = TRUE, 
+                            ntree = 500, 
+                            importance = TRUE, 
+                            balance.classes = TRUE,
+                            version = 2)
+
