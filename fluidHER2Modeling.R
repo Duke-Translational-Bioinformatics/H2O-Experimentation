99f767561752fc0d7952220ad95f2d6f38f59295
erichhuang <erich@mac.com>
2014-08-28 14:21:46 -0400

    Test modeling of HER2 status from DFCI dataset.

--- fluidHER2Modeling.R
+++ fluidHER2Modeling.R
@@ -1,4 +1,4 @@
-## fluidERModeling.R
+## fluidHER2Modeling.R
 ## erich.huang@duke.edu
 
 ## Experimenting with the H2O framework for machine learning
@@ -19,17 +19,19 @@ localH2O <- h2o.init(ip = 'localhost',
 # metaDF = clinical data frame with patients as rows and clinical covariates as columns
 load('~/Quantwork/ARTEMIS/bcSignaling/data/dfciData.RData')
 
-# For the sake of experimenting with the H2O framework, let's subset the data some.
-metaDF$series <- as.factor(metaDF$series)
-vdxMetaDF <- metaDF[metaDF$series == 'VDX', ]
-vdxExpDF <- fullDF[grep('VDX_', rownames(fullDF)), ]
+# Subset the data on samples annotated for HER2 status
+her2Ind <- grep('FALSE', is.na(metaDF$her2))
+her2SampIDs <- metaDF$samplename[her2Ind]
+
+vdxMetaDF <- metaDF[her2SampIDs, ]
+vdxExpDF <- fullDF[her2SampIDs, ]
 
 # Reduce the feature space for testing
 set.seed(140826)
 sampleGenes <- sample(colnames(vdxExpDF), size = 500)
 vdxExpDF <- vdxExpDF[ , sampleGenes]
 
-vdxExpDF <- data.frame(patients = rownames(vdxExpDF), er_status = vdxMetaDF[rownames(vdxExpDF), 'er'], vdxExpDF)
+vdxExpDF <- data.frame(patients = rownames(vdxExpDF), her2_status = vdxMetaDF[rownames(vdxExpDF), 'her2'], vdxExpDF)
 
 ## DIVIDE DATA INTO TRAIN AND TEST SETS
 set.seed(140827)
@@ -56,30 +58,39 @@ rfModel <- h2o.randomForest(x = 3:ncol(trainBC),
 ## PREDICT ON TEST SET
 yHatTest <- h2o.predict(rfModel, testBC)
 yHatTestDF <- as.data.frame(yHatTest)
-colnames(yHatTestDF) <- c('ER_Status', 'ER_Neg', 'ER_Prob')
+colnames(yHatTestDF) <- c('HER2_Call', 'HER2_Neg', 'HER2_Prob')
+yHatTestDF <- data.frame('HER2_True' = testData[ , 'her2_status'], yHatTestDF)
 
-ggplot(yHatTestDF, aes(factor(ER_Status), ER_Prob)) +
-  geom_boxplot(aes(fill = factor(ER_Status)), alpha = 0.4) +
-  geom_jitter(aes(colour = factor(ER_Status)), size = 4)
+ggplot(yHatTestDF, aes(factor(HER2_True), HER2_Prob)) +
+  geom_boxplot(aes(fill = factor(HER2_True)), alpha = 0.4) +
+  geom_jitter(aes(colour = factor(HER2_True)), size = 4) +
+  ggtitle('Distributed Random Forest HER2 Prediction on Held-Out Test Set\n')
 
-rfPerformance <- h2o.performance(data = yHatTest[ , 3], reference = yHatTest[ , 1])
+rfPerformance <- h2o.performance(data = yHatTest[ , 3], reference = testBC[ , 2])
 
 ## BUILD A DEEP LEARNING MODEL
 dlModel <- h2o.deeplearning(x = 3:ncol(trainBC),
                             y = 2,
                             data = trainBC,
+                            activation = 'TanhWithDropout',
+                            input_dropout_ratio = 0,
+                            hidden_dropout_ratios = c(0.5, 0.5, 0.5),
+                            hidden = c(50, 50, 50),
+                            epochs = 500,
                             classification = TRUE,
                             balance_classes = TRUE)
   
 ## PREDICT ON TEST SET
 dlYHatTest <- h2o.predict(dlModel, testBC)
 dlYHatTestDF <- as.data.frame(dlYHatTest)  
-colnames(dlYHatTestDF) <- c('ER_Status', 'ER_Neg', 'ER_Prob')
+colnames(dlYHatTestDF) <- c('HER2_Status', 'HER2_Neg', 'HER2_Prob')
+dlYHatTestDF <- data.frame('HER2_True' = testData[ , 'her2_status'], dlYHatTestDF)
 
-ggplot(dlYHatTestDF, aes(factor(ER_Status), ER_Prob)) +
-  geom_boxplot(aes(fill = factor(ER_Status)), alpha = 0.4) +
-  geom_jitter(aes(colour = factor(ER_Status)), size = 4)
+ggplot(dlYHatTestDF, aes(factor(HER2_Status), HER2_Prob)) +
+  geom_boxplot(aes(fill = factor(HER2_Status)), alpha = 0.4) +
+  geom_jitter(aes(colour = factor(HER2_Status)), size = 4) +
+  ggtitle('Regularized Deep Learning HER2 Prediction on Held-Out Test Set\n')
 
-dlPerformance <- h2o.performance(data = dlYHatTest[ , 3], reference = dlYHatTest[ , 1])
+dlPerformance <- h2o.performance(data = dlYHatTest[ , 3], reference = testBC[ , 2])
 
 
